9b62688e21f1a49c173d07fe83ca0d4aaeda7acc
Laurent Montel <montel@kde.org>
2009-02-28 16:43:43 +0000

    Fix extract messages
    svn path=/trunk/extragear/utils/yakuake/; revision=933316

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT rc.cpp app/*.cpp app/*.h -o $podir/yakuake.pot
+$XGETTEXT rc.cpp app/config/*.cpp app/*.cpp app/*.h -o $podir/yakuake.pot
 rm -f rc.cpp
