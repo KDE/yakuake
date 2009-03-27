0db59a24097c787f4ac12859ef5d12e14377e7b3
Eike Hein <hein@kde.org>
2009-03-27 18:08:44 +0000

    Amend credits.
    svn path=/trunk/extragear/utils/yakuake/; revision=945567

--- app/main.cpp
+++ app/main.cpp
@@ -58,7 +58,7 @@ int main (int argc, char *argv[])
     aboutData.addCredit(ki18nc("@info:credit", "Daniel 'suslik' D."), 
         ki18nc("@info:credit", "Plastik skin"), "dd@accentsolution.com");
     aboutData.addCredit(ki18nc("@info:credit", "Juan Carlos Torres"),
-        ki18nc("@info:credit", "Tab bar drag and drop support"), "carlosdgtorres@gmail.com");
+        ki18nc("@info:credit", "Tab bar drag and drop support, Prevent Closing toggle"), "carlosdgtorres@gmail.com");
 
     KCmdLineArgs::init(argc, argv, &aboutData);
 
