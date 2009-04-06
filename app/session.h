96d389a51d593606821cfad7dd8cf5b039bf0243
Eike Hein <hein@kde.org>
2009-04-06 22:12:10 +0000

    Add copyright lines for Jucato.
    svn path=/trunk/extragear/utils/yakuake/; revision=950359

--- app/session.h
+++ app/session.h
@@ -1,5 +1,6 @@
 /*
   Copyright (C) 2008-2009 by Eike Hein <hein@kde.org>
+  Copyright (C) 2009 by Juan Carlos Torres <carlosdgtorres@gmail.com>
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
