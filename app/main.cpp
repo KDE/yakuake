96bd6f3eca3ab11fccc3d0bc44133d74e6df3c15
Eike Hein <hein@kde.org>
2009-05-19 03:39:38 +0000

    Update for release.
    svn path=/trunk/extragear/utils/yakuake/; revision=969833

--- app/main.cpp
+++ app/main.cpp
@@ -42,7 +42,7 @@ int main (int argc, char *argv[])
     KAboutData aboutData("yakuake",
         0,
         ki18nc("@title", "<application>Yakuake</application>"),
-        "2.9.5+",
+        "2.9.6",
         ki18nc("@title", "A drop-down terminal emulator based on KDE Konsole technology."),
         KAboutData::License_GPL,
         ki18nc("@info:credit", "(c) 2008-2009 The Yakuake Team"),
