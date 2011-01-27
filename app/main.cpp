b259cbcb002d5199e8bff7647fecd30bb86fc53c
Eike Hein <hein@kde.org>
2011-01-27 18:02:00 +0100

    Adjust for release.

--- app/main.cpp
+++ app/main.cpp
@@ -35,7 +35,7 @@ int main (int argc, char *argv[])
     KAboutData aboutData("yakuake",
         0,
         ki18nc("@title", "<application>Yakuake</application>"),
-        "2.9.7+",
+        "2.9.8",
         ki18nc("@title", "A drop-down terminal emulator based on KDE Konsole technology."),
         KAboutData::License_GPL,
         ki18nc("@info:credit", "(c) 2008-2011 The Yakuake Team"),
