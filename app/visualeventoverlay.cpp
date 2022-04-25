c9bf10100547246e1698899fa09a49a1e3109c84
Nicolas Fella <nicolas.fella@gmx.de>
2022-04-26 01:38:48 +0200

    Use correct header for KStatefulBrush
    It used to be part of KColorScheme, and still is for compatibility
    
    Use the proper location for future-proofing

--- app/visualeventoverlay.cpp
+++ app/visualeventoverlay.cpp
@@ -10,6 +10,10 @@
 #include "terminal.h"
 
 #include <KColorScheme>
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush>
+#endif
 
 #include <QPainter>
 #include <QTimer>
