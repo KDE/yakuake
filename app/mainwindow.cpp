0489b34de4e10ba37c8d91cd2b64d7c28f86c90a
Laurent Montel <montel@kde.org>
2022-05-24 07:09:51 +0200

    fix includes for building against qt6

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -48,7 +48,11 @@
 #include <QWindow>
 
 #if HAVE_X11
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 #include <QX11Info>
+#else
+#include <private/qtx11extras_p.h>
+#endif
 
 #include <X11/Xlib.h>
 #include <fixx11h.h>
