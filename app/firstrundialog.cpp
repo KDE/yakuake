19b243e4d3fb5c54e1ff8b7c3428cec51e28045b
Laurent Montel <montel@kde.org>
2024-08-24 10:29:27 +0200

    Add parent

--- app/firstrundialog.cpp
+++ app/firstrundialog.cpp
@@ -19,9 +19,8 @@ FirstRunDialog::FirstRunDialog(MainWindow *mainWindow)
     , m_mainWindow(mainWindow)
 {
     setWindowTitle(xi18nc("@title:window", "First Run"));
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    setLayout(mainLayout);
-    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
     QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
